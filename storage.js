61090b0a7b9a3395ceb282bceddb25d98ba1a26e
engmbadr808-max <engmbadr808@gmail.com>
2026-03-02 22:36:32 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,15 @@
 
+const Storage = {
+
+    load(){
+        const data = localStorage.getItem("mosqueSettings");
+        return data ? JSON.parse(data) : CONFIG;
+    },
+
+    save(data){
+        localStorage.setItem(
+            "mosqueSettings",
+            JSON.stringify(data)
+        );
+    }
+};
