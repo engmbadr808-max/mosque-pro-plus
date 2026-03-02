6198a41c3483ffb3115a253066cb9cf6588641db
engmbadr808-max <engmbadr808@gmail.com>
2026-03-02 22:39:09 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,17 @@
+let settings=Storage.load();
 
+document.getElementById("mosque").value=settings.mosqueName;
+document.getElementById("dhuhr").value=settings.iqamaDelay.Dhuhr;
+
+function save(){
+
+settings.mosqueName=
+document.getElementById("mosque").value;
+
+settings.iqamaDelay.Dhuhr=
+Number(document.getElementById("dhuhr").value);
+
+Storage.save(settings);
+
+alert("تم حفظ الإعدادات ✅");
+}
