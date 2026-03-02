83c2754cafc226bf4831a230b0020f07ca07407b
engmbadr808-max <engmbadr808@gmail.com>
2026-03-02 22:36:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,27 @@
 
+const CONFIG = {
+
+    mosqueName: "مسجد النور",
+
+    city: "Riyadh",
+    country: "Saudi Arabia",
+
+    beforeAdhanAlert: 10,
+
+    iqamaDelay:{
+        Fajr:20,
+        Dhuhr:10,
+        Asr:10,
+        Maghrib:5,
+        Isha:10
+    },
+
+    azkar:[
+        "استغفر الله العظيم",
+        "سبحان الله وبحمده",
+        "لا إله إلا الله وحده لا شريك له",
+        "اللهم صل وسلم على نبينا محمد"
+    ],
+
+    adhanAudio:"assets/adhan.mp3"
+};
